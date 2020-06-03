b7524752efa77bea6ce0fadf9f52dd64174976df
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2020-06-03 11:48:40 +0545

    OOPS

--- example.js
+++ example.js
@@ -1,103 +1,78 @@
-/*
-1. Function Expression
-2. Anonymous Function
-3. Arrow Function
-4. IIFE
-*/
-// Function Expression
-var sum = function calculate(a,b,c){
-    return a+b+c;
+var Top = function(name){
+    this.a = 10; 
+    this.name = name;
+}
+Top.prototype.show_name = function(){
+    return this.name;
 }
 
-// Anonymous Function
-var sub = function (x,y,z){
-    return x-y-z;
+var Middle = function(name){
+    Top.call(this); // Instance Member Inheritance
+    this.b = 20;
+    this.name = name;
 }
 
-// Arrow Function
-var multiply = (a,b,c) => {return a*b*c};
-
-// Immediately Invoked Function Expression
-(
-   function (a,b){
-       return a/b;
-   }
-)(4,2);
-
-/*
- Two ways to create object-- Using Object literal and using Object Constructor
-*/
-var mobile = {};  // Using Object Literal
-var car = new Object()  // Uisng Object Constructor
-// Creating Object Properties
-mobile["ram"] = '8gb';
-car.color = "black";
-// Accessing Object Properties
-console.log(mobile.ram);
-console.log(car.color);
-// Deleting Object Properties
-delete mobile.ram;
-delete car.color;
-// Creating Methods
-mobile["msg"]= function(){
-    console.log("I am a smartphone");
+var Bottom = function(name){
+    Middle.call(this);  // Instance Member Inheritance
+    this.c = 30;
+    this.name = name;
 }
-// Deleting Methods
-delete mobile.msg;
 
-// Factory Function -- When The function returns a object
-function Get_Specification(){
-    return {
-        ram :"8 GB",
-        display : "Full HD plus",
+// Prototype Inheritance
+Middle.prototype = Object.create(Top.prototype);
+Bottom.prototype = Object.create(Middle.prototype);
+
+// Creating Objects
+var T = new Top("T");
+var M = new Middle("M");
+var B = new Bottom("B");
+
+console.log(T.show_name());
+console.log(M.show_name());
+console.log(B.show_name());
+console.log(M.a);
+console.log(B.b);
+console.log(B.a);
+
+//--------------------------------------------------------------------------------------------------------------------------------------------
+
+class Top_1 {
+    constructor(name){
+        this.x = 100;
+        this.name = name;
+    }
+    show_my_name(){
+        return this.name;
     }
 }
-general_spec=Get_Specification()
-console.log(general_spec.ram);
 
-// Creating a Constructor
-function Daily_requirements(food_name, min_money){
-    this.food_name = food_name;
-    this.min_money = min_money;
-    this.check_requirement = function (){
-        if (this.min_money > 10000){
-            return "Requirement is high";
-        }
-        else {
-            return "Requirements is low";
-        };
-    };
-};
+class Middle_1 extends Top_1{
+    constructor(name){
+        super();
+        this.y = 200;
+        this.name = name;
+    }    
+}
 
-var ram = new Daily_requirements("Rice" , 2000);
-var shyam = new Daily_requirements("Roti", 5000);
-var gita = new Daily_requirements("Rice and Roti", 11000);
-console.log(ram.food_name);
-console.log(shyam.min_money);
-console.log(gita.check_requirement());
+class Bottom_1 extends Middle_1{
+    constructor(name){
+        super();
+        this.z = 300;
+        this.name = name;
+    }
+} 
 
+var T_1 = new Top_1("T_1");
+var M_1 = new Middle_1("M_1");
+var B_1 = new Bottom_1("B_1");
+console.log(T_1.show_my_name());
+console.log(M_1.show_my_name());
+console.log(B_1.show_my_name());
+console.log(T_1.x);
+console.log(M_1.y);
+console.log(B_1.z);
+console.log(M_1.x);
+console.log(B_1.y);
+console.log(B_1.x);
 
-var class1 = function (a){
-    this.a = a;
-};
-
-var class2 = function (b) {
-    class1.call(this);
-    this.b = b;
-};
-
-var class3 = function (c) {
-    class1.call(this);
-    this.c = c;
-};
-
-var obj1 = new class1(10);
-var obj2 = new class2(20);
-var obj3 = new class3(30);
-
-console.log(obj1.a);
-console.log(obj2.b);
-console.log(obj3.c);
-console.log(obj2.a);
-console.log(obj3.a);
-
+// -------------------------------------------------------------------------------------------------------------------------------------------
